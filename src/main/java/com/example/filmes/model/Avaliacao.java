47cf9e332892270db3b30892457965dba4e4db90
herobruno <brunoss2118@gmail.com>
2025-05-28 14:14:58 -0300

    feat: entidade avaliação

---
+++ src/main/java/com/example/filmes/model/Avaliacao.java
@@ -0,0 +1,64 @@
+package com.example.filmes.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "avaliacoes")
+public class Avaliacao {
+    @Id
+    private String id;
+
+    public Avaliacao(String autor, String comentario, int nota, String filmeId) {
+        this.autor = autor;
+        this.comentario = comentario;
+        this.nota = nota;
+        this.filmeId = filmeId;
+    }
+
+    private String autor;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getFilmeId() {
+        return filmeId;
+    }
+
+    public void setFilmeId(String filmeId) {
+        this.filmeId = filmeId;
+    }
+
+    private String comentario;
+    private int nota;
+
+    private String filmeId;
+}
